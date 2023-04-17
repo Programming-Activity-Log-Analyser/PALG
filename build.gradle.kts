25666b06c475ca20be488df3a947c697f48d3552
Rene Kütt <Rene.Kytt@nortal.com>
2023-04-17 21:24:32 +0300

    migrated .gradle files to .gradle.kts. Added dependencies to kotlin-logging, logback-classic and gson

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij") version "1.13.3"
+    id("org.jetbrains.kotlin.jvm") version "1.8.20"
+}
+
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
+    implementation("ch.qos.logback:logback-classic:1.4.6")
+    implementation("com.google.code.gson:gson:2.10.1")
+}
+
+intellij {
+    version.set("2022.2.5")
+}
+
+tasks {
+    buildSearchableOptions {
+        enabled = false
+    }
+
+    compileKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+}
+
+
